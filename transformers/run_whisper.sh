48af1c93963fbcd70085a3b5e4059045a990914b
Somshubra Majumdar <titu1994@gmail.com>
2023-07-31 13:48:18 -0700

    Manifest based Scoring (#3)
    * Update eval script for Fast Conformer NeMo models to support write and post-scoring
    
    * Add evaluate helper
    
    * Alias manifest utils in data utils
    
    * Update eval script for HF models to support write and post-scoring
    
    * Add comments
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Fix detection of dataset id
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    * Add checks for empty string in model filtering for eval script
    
    Signed-off-by: smajumdar <titu1994@gmail.com>
    
    ---------
    
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- transformers/run_whisper.sh
+++ transformers/run_whisper.sh
@@ -2,10 +2,19 @@
 
 export PYTHONPATH="..":$PYTHONPATH
 
+MODEL_ID="openai/whisper-tiny"
+
 python run_eval.py \
-	--model_id="openai/whisper-tiny" \
+	--model_id=$MODEL_ID \
 	--dataset_path="librispeech_asr" \
 	--dataset="other" \
 	--split="test" \
 	--batch_size=32 \
 	--device=0 
+
+
+# Evaluate results
+RUNDIR=`pwd` && \
+cd ../normalizer && \
+python -c "import eval_utils; eval_utils.score_results('${RUNDIR}/results', '${MODEL_ID}')" && \
+cd $RUNDIR
