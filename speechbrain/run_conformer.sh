8bec40aa18442890ed3c0b7f7f6eefeb1633a89c
Adel Moumen <adelmoumen.pro@gmail.com>
2023-07-31 16:31:44 +0200

    conformer.sh

--- speechbrain/run_conformer.sh
+++ speechbrain/run_conformer.sh
@@ -3,10 +3,10 @@
 export PYTHONPATH="..":$PYTHONPATH
 
 python run_eval.py \
-	--model_id="nvidia/stt_en_conformer_transducer_large" \
+	--source="asr-conformer-transformerlm-librispeech" \
+    --speechbrain_pretrained_class_name="EncoderDecoderASR" \
 	--dataset_path="librispeech_asr" \
 	--dataset="other" \
 	--split="test" \
 	--device=0 \
-	--batch_size=16 \
-	--max_eval_samples=-1
+	--batch_size=16
