b1d8c0b45e9b206a6253a32d9dd9da357e0e79d7
Adel Moumen <adelmoumen.pro@gmail.com>
2023-08-01 12:42:01 +0200

    update scripts

--- speechbrain/run_crdnn_transformerlm.sh
+++ speechbrain/run_crdnn_transformerlm.sh
@@ -2,11 +2,30 @@
 
 export PYTHONPATH="..":$PYTHONPATH
 
+SOURCE="speechbrain/asr-crdnn-transformerlm-librispeech"
+
 python run_eval.py \
-	--source="asr-crdnn-transformerlm-librispeech" \
+	--source=$SOURCE \
+    --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+	--dataset_path="librispeech_asr" \
+	--dataset="clean" \
+	--split="test" \
+	--device=0 \
+	--batch_size=4 \
+	--max_eval_samples=-1
+
+python run_eval.py \
+	--source=$SOURCE \
     --speechbrain_pretrained_class_name="EncoderDecoderASR" \
 	--dataset_path="librispeech_asr" \
 	--dataset="other" \
 	--split="test" \
 	--device=0 \
-	--batch_size=4
+	--batch_size=4 \
+	--max_eval_samples=-1
+
+# Evaluate results
+RUNDIR=`pwd` && \
+cd ../normalizer && \
+python -c "import eval_utils; eval_utils.score_results('${RUNDIR}/results', '${MODEL_ID}')" && \
+cd $RUNDIR
