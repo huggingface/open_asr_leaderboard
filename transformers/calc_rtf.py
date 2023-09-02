7f11b875980b429be822073b2eefcd713879a023
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-09-02 17:08:01 +0200

    Initial commit - RTF script

--- transformers/calc_rtf.py
+++ transformers/calc_rtf.py
@@ -2,7 +2,7 @@ import time
 from transformers import pipeline
 import librosa
 
-device = "cuda:0"
+device = "cpu"
 model = "openai/whisper-tiny.en"
 n_batches = 5
 warmup_batches = 3
@@ -12,26 +12,28 @@ audio_file = "4469669.mp3"
 max_len = 600 #10 minutes
 
 def pre_process_audio(audio_file, sr, max_len):
-    _, sr = librosa.load(audio_file, sr=sr)
-    _mono = librosa.to_mono(_)
-    audio_len = int(max_len * sr)
-    audio_arr = _mono[audio_len]
-    return {"raw": audio_arr, "sampling_rate" = 16000}, audio_len
+    _, _sr = librosa.load(audio_file, sr=sr)
+    audio_len = int(max_len * _sr)
+    audio_arr = _[:audio_len]
+    return {"raw": audio_arr, "sampling_rate": _sr}, audio_len
 
-audio_dict, audio_len = pre_process_audio(audio_file)
+audio_dict, audio_len = pre_process_audio(audio_file, 16000, max_len)
 
 rtfs = []
 for i in range(3):
-    for _ in range(n_batchs + warmup_batches):
+    print(f'outer_loop -> {i}')
+    total_time = 0.0
+    for _ in range(n_batches + warmup_batches):
+        print(f"batch_num -> {_}")
         start = time.time()
-        transcription = pipe(audio, chunk_length_s=15)
+        transcription = pipe({"raw":audio_dict["raw"], "sampling_rate": audio_dict["sampling_rate"]}, chunk_length_s=10)
         end = time.time()
         if _ >= warmup_batches:
             total_time += end - start
         
-        rtf = (total_time/nbatches) / (audio_len / 16000)
+    rtf = (total_time/n_batches) / (audio_len / 16000)
     rtfs.append(rtf)
 
-print(f'RTF: {rtfs}')
+print(f'all RTFs: {rtfs}')
 rtf_val = sum(rtfs)/len(rtfs)
 print(f'avg. RTF: {rtf_val}')
