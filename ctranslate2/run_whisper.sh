ca92caf981d2ec229c7fd09f71cc41895ccd9dcf
Thomas Chaigneau <t.chaigneau.tc@gmail.com>
2023-09-11 12:10:58 +0200

    Add support for `ctranslate2` whisper models (#10)
    * add ctranslate2 eval
    
    * correct calc_rtf name for all files
    
    * add missing regex dependency
    
    * fix cuda index
    
    * add DEVICE_INDEX as variable
    
    * add hub login
    
    * remove regex included in transformers
    
    * fix inference
    
    * use_auth_token -> token
    
    * add compute type for evaluation
    
    * scripts corrections
    
    * fix typo
    
    * update shell script url

---
+++ ctranslate2/run_whisper.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+export PYTHONPATH="..":$PYTHONPATH
+
+MODEL_IDs=("guillaumekln/faster-whisper-tiny.en" "guillaumekln/faster-whisper-small.en" "guillaumekln/faster-whisper-base.en" "guillaumekln/faster-whisper-medium.en" "guillaumekln/faster-whisper-large-v1" "guillaumekln/faster-whisper-large-v2")
+BATCH_SIZE=1
+DEVICE_INDEX=0
+
+num_models=${#MODEL_IDs[@]}
+
+for (( i=0; i<${num_models}; i++ ));
+do
+    MODEL_ID=${MODEL_IDs[$i]}
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="ami" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="earnings22" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="gigaspeech" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="librispeech" \
+        --split="test.clean" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="librispeech" \
+        --split="test.other" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="spgispeech" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="tedlium" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="voxpopuli" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="https://huggingface.co/datasets/hf-audio/esb-datasets-test-only" \
+        --dataset="common_voice" \
+        --split="test" \
+        --device=${DEVICE_INDEX} \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1
+
+    # Evaluate results
+    RUNDIR=`pwd` && \
+    cd ../normalizer && \
+    python -c "import eval_utils; eval_utils.score_results('${RUNDIR}/results', '${MODEL_ID}')" && \
+    cd $RUNDIR
+
+done
