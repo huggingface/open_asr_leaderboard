729e46dd679cb6928b2e513711150f789cb5fdce
sanchit-gandhi <sanchit@huggingface.co>
2024-02-07 17:06:05 +0000

    add short form rtf

--- transformers/run_eval.py
+++ transformers/run_eval.py
@@ -4,6 +4,8 @@ import os
 from transformers import pipeline
 import evaluate
 from normalizer import data_utils
+import time
+from tqdm import tqdm
 
 wer_metric = evaluate.load("wer")
 
@@ -15,35 +17,86 @@ def dataset_iterator(dataset):
 
 def main(args):
     asr_pipe = pipeline(
-        "automatic-speech-recognition", model=args.model_id, device=args.device
+        "automatic-speech-recognition", model=args.model_id, device=args.device, batch_size=args.batch_size
     )
+
+    if asr_pipe.model.can_generate():
+        gen_kwargs = {"max_new_tokens": 128}
+        # for multilingual Whisper-checkpoints we see a definitive WER boost by setting the language and task args
+        if (
+            hasattr(asr_pipe.model.generation_config, "is_multilingual")
+            and asr_pipe.model.generation_config.is_multilingual
+        ):
+            gen_kwargs["language"] = "en"
+            gen_kwargs["task"] = "transcribe"
+    else:
+        gen_kwargs = None
 
     dataset = data_utils.load_data(args)
 
     if args.max_eval_samples is not None and args.max_eval_samples > 0:
-        print(f"Subsampling dataset to first {args.max_eval_samples} samples !")
+        print(f"Subsampling dataset to first {args.max_eval_samples} samples!")
         dataset = dataset.take(args.max_eval_samples)
 
     dataset = data_utils.prepare_data(dataset)
 
-    predictions = []
-    references = []
+    def benchmark(batch):
+        # get audio stats
+        audio = [sample["array"] for sample in batch["audio"]]
+        batch["audio_length"] = [len(sample) / 16_000 for sample in audio]
+        minibatch_size = len(audio)
 
-    # run streamed inference
-    for out in asr_pipe(dataset_iterator(dataset), batch_size=args.batch_size):
-        predictions.append(data_utils.normalizer(out["text"]))
-        references.append(out["reference"][0])
+        # timing step
+        start_time = time.time()
+        result = asr_pipe(batch["audio"], generate_kwargs=gen_kwargs)
+        # normalize by minibatch size since we want the per-sample time
+        runtime = (time.time() - start_time) / minibatch_size
+        batch["transcription_time"] = [runtime for _ in range(minibatch_size)]
+
+        # normalize transcriptions with English normalizer
+        batch["predictions"] = [data_utils.normalizer(pred["text"]) for pred in result]
+        batch["references"] = batch["norm_text"]
+        return batch
+
+    dataset = dataset.map(
+        benchmark, batch_size=args.batch_size, batched=True, remove_columns=["audio"]
+    )
+
+    all_results = {
+        "audio_length": [],
+        "transcription_time": [],
+        "predictions": [],
+        "references": [],
+    }
+    result_iter = iter(dataset)
+    for result in tqdm(result_iter, desc="Samples"):
+        for key in all_results:
+            all_results[key].append(result[key])
 
     # Write manifest results
     manifest_path = data_utils.write_manifest(
-        references, predictions, args.model_id, args.dataset_path, args.dataset, args.split
+        all_results["references"],
+        all_results["predictions"],
+        args.model_id,
+        args.dataset_path,
+        args.dataset,
+        args.split,
+        audio_length=all_results["audio_length"],
+        transcription_time=all_results["transcription_time"],
     )
     print("Results saved at path:", os.path.abspath(manifest_path))
 
-    wer = wer_metric.compute(references=references, predictions=predictions)
+    wer = wer_metric.compute(
+        references=all_results["references"], predictions=all_results["predictions"]
+    )
     wer = round(100 * wer, 2)
-
     print("WER:", wer, "%")
+
+    transcription_time = sum(all_results["transcription_time"])
+    audio_length = sum(all_results["audio_length"])
+    rtfx = audio_length / transcription_time
+    rtfx = round(rtfx, 4)
+    print("RTFX:", rtfx)
 
 
 if __name__ == "__main__":
@@ -56,14 +109,17 @@ if __name__ == "__main__":
         help="Model identifier. Should be loadable with 🤗 Transformers",
     )
     parser.add_argument(
-        '--dataset_path', type=str, default='esb/datasets', help='Dataset path. By default, it is `esb/datasets`'
+        "--dataset_path",
+        type=str,
+        default="esb/datasets",
+        help="Dataset path. By default, it is `esb/datasets`",
     )
     parser.add_argument(
         "--dataset",
         type=str,
         required=True,
         help="Dataset name. *E.g.* `'librispeech_asr` for the LibriSpeech ASR dataset, or `'common_voice'` for Common Voice. The full list of dataset names "
-            "can be found at `https://huggingface.co/datasets/esb/datasets`"
+        "can be found at `https://huggingface.co/datasets/esb/datasets`",
     )
     parser.add_argument(
         "--split",
@@ -91,7 +147,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--no-streaming",
-        dest='streaming',
+        dest="streaming",
         action="store_false",
         help="Choose whether you'd like to download the entire dataset or stream it during the evaluation.",
     )
