db75efa8730d9fab111caf30f7e8c52e1c3c5d2d
sanchit-gandhi <sanchit@huggingface.co>
2024-08-07 14:06:01 +0100

    fix indentation

--- speechbrain/run_wav2vec2_commonvoice.sh
+++ speechbrain/run_wav2vec2_commonvoice.sh
@@ -5,24 +5,24 @@ export PYTHONPATH="..":$PYTHONPATH
 SOURCE="speechbrain/asr-wav2vec2-commonvoice-en"
 
 python run_eval.py \
-	--source=$SOURCE \
-    --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
   --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
   --dataset="librispeech" \
   --split="test.clean" \
-	--device=0 \
-	--batch_size=16 \
-	--max_eval_samples=-1
+  --device=0 \
+  --batch_size=16 \
+  --max_eval_samples=-1
 
 python run_eval.py \
-	--source=$SOURCE \
-    --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
   --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
   --dataset="librispeech" \
   --split="test.other" \
-	--device=0 \
-	--batch_size=16 \
-	--max_eval_samples=-1
+  --device=0 \
+  --batch_size=16 \
+  --max_eval_samples=-1
 
 # Evaluate results
 RUNDIR=`pwd` && \
