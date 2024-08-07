a9a880895dbb881d6de1c0ef7a2c397e4dc0213a
sanchit-gandhi <sanchit@huggingface.co>
2024-08-07 10:02:03 +0100

    update faster whisper for rtfx

--- ctranslate2/run_eval.py
+++ ctranslate2/run_eval.py
@@ -1,6 +1,7 @@
 """Run evaluation for ctranslate2 whisper models."""""
 import argparse
 import os
+import time
 
 import evaluate
 from faster_whisper import WhisperModel
@@ -9,20 +10,6 @@ from tqdm import tqdm
 from normalizer import data_utils
 
 wer_metric = evaluate.load("wer")
-
-
-def dataset_iterator(dataset) -> dict:
-    """
-    Iterate over the dataset and yield a dictionary with the audio and reference text.
-
-    Args:
-        dataset: dataset to iterate over
-
-    Returns:
-        dictionary: {"audio": audio, "reference": reference}
-    """
-    for item in dataset:
-        yield {**item["audio"], "reference": item["norm_text"]}
 
 
 def main(args) -> None:
@@ -34,38 +21,69 @@ def main(args) -> None:
         device_index=args.device
     )
 
-    dataset = data_utils.load_data(args)
-
-    if args.max_eval_samples is not None and args.max_eval_samples > 0:
-        print(f"Subsampling dataset to first {args.max_eval_samples} samples !")
-        dataset = dataset.take(args.max_eval_samples)
-
-    dataset = data_utils.prepare_data(dataset)
-
-    predictions = []
-    references = []
-
-    # Run inference
-    for batch in tqdm(dataset_iterator(dataset), desc=f"Evaluating {args.model_id}"):
+    def benchmark(batch):
+        start_time = time.time()
         segments, _ = asr_model.transcribe(batch["array"], language="en")
         outputs = [segment._asdict() for segment in segments]
-        transcription = data_utils.normalizer(
-            "".join([segment["text"] for segment in outputs])
-        ).strip()
+        batch["transcription_time_s"] = time.time() - start_time
+        batch["predictions"] = data_utils.normalizer("".join([segment["text"] for segment in outputs])).strip()
+        batch["references"] = batch["norm_text"]
+        return batch
 
-        predictions.append(transcription)
-        references.append(batch["reference"])
+    if args.warmup_steps is not None:
+        dataset = data_utils.load_data(args)
+        dataset = data_utils.prepare_data(dataset)
 
-    # Write manifest results
+        if args.streaming:
+            warmup_dataset = dataset.take(args.warmup_steps)
+        else:
+            warmup_dataset = dataset.select(range(min(args.warmup_steps, len(dataset))))
+        warmup_dataset = iter(warmup_dataset.map(benchmark, remove_columns=["audio"]))
+
+        for _ in tqdm(warmup_dataset, desc="Warming up..."):
+            continue
+
+    dataset = data_utils.load_data(args)
+    if args.max_eval_samples is not None and args.max_eval_samples > 0:
+        print(f"Subsampling dataset to first {args.max_eval_samples} samples!")
+        if args.streaming:
+            dataset = dataset.take(args.max_eval_samples)
+        else:
+            dataset = dataset.select(range(min(args.max_eval_samples, len(dataset))))
+    dataset = data_utils.prepare_data(dataset)
+
+    dataset = dataset.map(benchmark, remove_columns=["audio"])
+
+    all_results = {
+        "audio_length_s": [],
+        "transcription_time_s": [],
+        "predictions": [],
+        "references": [],
+    }
+    result_iter = iter(dataset)
+    for result in tqdm(result_iter, desc="Samples..."):
+        for key in all_results:
+            all_results[key].append(result[key])
+
+    # Write manifest results (WER and RTFX)
     manifest_path = data_utils.write_manifest(
-        references, predictions, args.model_id, args.dataset_path, args.dataset, args.split
+        all_results["references"],
+        all_results["predictions"],
+        args.model_id,
+        args.dataset_path,
+        args.dataset,
+        args.split,
+        audio_length=all_results["audio_length_s"],
+        transcription_time=all_results["transcription_time_s"],
     )
     print("Results saved at path:", os.path.abspath(manifest_path))
 
-    wer = wer_metric.compute(references=references, predictions=predictions)
+    wer = wer_metric.compute(
+        references=all_results["references"], predictions=all_results["predictions"]
+    )
     wer = round(100 * wer, 2)
-
-    print("WER:", wer, "%")
+    rtfx = round(sum(all_results["audio_length_s"]) / sum(all_results["transcription_time_s"]), 2)
+    print("WER:", wer, "%", "RTFx:", rtfx)
 
 
 if __name__ == "__main__":
@@ -75,7 +93,7 @@ if __name__ == "__main__":
         "--model_id",
         type=str,
         required=True,
-        help="Model identifier. Should be loadable with 🤗 Transformers",
+        help="Model identifier. Should be loadable with faster-whisper",
     )
     parser.add_argument(
         '--dataset_path', type=str, default='esb/datasets', help='Dataset path. By default, it is `esb/datasets`'
@@ -116,6 +134,12 @@ if __name__ == "__main__":
         dest='streaming',
         action="store_false",
         help="Choose whether you'd like to download the entire dataset or stream it during the evaluation.",
+    )
+    parser.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=5,
+        help="Number of warm-up steps to run before launching the timed runs.",
     )
     args = parser.parse_args()
     parser.set_defaults(streaming=False)
