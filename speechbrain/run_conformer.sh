77960f50a005d0919f8c0e281ea7cb9cd3a4f37b
Titouan Parcollet/Embedded AI /SRUK/Engineer/Samsung Electronics <t.parcollet@sruk-ccn4.eu.corp.samsungelectronics.net>
2025-02-06 19:33:25 +0000

    fix comments

--- speechbrain/run_conformer.sh
+++ speechbrain/run_conformer.sh
@@ -3,6 +3,8 @@
 export PYTHONPATH="..":$PYTHONPATH
 
 SOURCE="speechbrain/asr-conformer-largescaleasr"
+BATCH_SIZE=8
+DEVICE_ID=0
 
 # Run with CTC+Attn
 python run_eval.py \
@@ -11,10 +13,88 @@ python run_eval.py \
   --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
   --dataset="librispeech" \
   --split="test.clean" \
-  --device=7 \
-  --batch_size=4 \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
   --max_eval_samples=-1 \
   --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="librispeech" \
+  --split="test.other" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="ami" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="spgispeech" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="tedlium" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="earnings22" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="gigaspeech" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="voxpopuli" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+
 
 # Run with Attn only
 python run_eval.py \
@@ -23,11 +103,97 @@ python run_eval.py \
   --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
   --dataset="librispeech" \
   --split="test.clean" \
-  --device=7 \
-  --batch_size=4 \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
   --max_eval_samples=-1 \
   --beam_size=40 \
   --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="librispeech" \
+  --split="test.other" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="ami" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="spgispeech" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="tedlium" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="earnings22" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="gigaspeech" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+python run_eval.py \
+  --source=$SOURCE \
+  --speechbrain_pretrained_class_name="EncoderDecoderASR" \
+  --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
+  --dataset="voxpopuli" \
+  --split="test" \
+  --device=${DEVICE_ID} \
+  --batch_size=${BATCH_SIZE} \
+  --max_eval_samples=-1 \
+  --beam_size=40 \
+  --ctc_weight_decode=0
+
+
 
 # Evaluate results
 RUNDIR=`pwd` && \
