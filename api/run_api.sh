a7ab20c9e2a03e8aee4002829092e05e9f9acd8a
agupta54 <anirudhgupta12@gmail.com>
2025-06-26 18:31:38 +0530

    Adding support for ganai ASR

--- api/run_api.sh
+++ api/run_api.sh
@@ -6,6 +6,7 @@ export OPENAI_API_KEY="your_api_key"
 export ASSEMBLYAI_API_KEY="your_api_key"
 export ELEVENLABS_API_KEY="your_api_key"
 export REVAI_API_KEY="your_api_key"
+export GANAI_API_KEY="your_api_key"
 
 MODEL_IDs=(
     "openai/gpt-4o-transcribe"
@@ -16,7 +17,9 @@ MODEL_IDs=(
     "revai/machine" # please use --use_url=True
     "revai/fusion" # please use --use_url=True
     "speechmatics/enhanced"
+    "ganai/asr-v1" # please use --num_workers=5
 )
+
 
 num_models=${#MODEL_IDs[@]}
 
@@ -27,48 +30,56 @@ do
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="ami" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="earnings22" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="gigaspeech" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path "hf-audio/esb-datasets-test-only-sorted" \
         --dataset "librispeech" \
         --split "test.clean" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path "hf-audio/esb-datasets-test-only-sorted" \
         --dataset "librispeech" \
         --split "test.other" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="spgispeech" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="tedlium" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
 
     python run_eval.py \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="voxpopuli" \
         --split="test" \
+        --max_workers=5 \
         --model_name ${MODEL_ID}
     
     # Evaluate results
