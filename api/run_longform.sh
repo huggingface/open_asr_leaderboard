452d388f864c479bf9e4c68385edb9ae95e961f7
Deep-unlearning <steven@huggingface.co>
2025-10-13 07:49:39 +0000

    add longform script

---
+++ api/run_longform.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+export PYTHONPATH="..":$PYTHONPATH
+
+export OPENAI_API_KEY="your_api_key"
+export ASSEMBLYAI_API_KEY="your_api_key"
+export ELEVENLABS_API_KEY="your_api_key"
+export REVAI_API_KEY="your_api_key"
+export AQUAVOICE_API_KEY="your_api_key"
+
+MODEL_IDs=(
+    "openai/gpt-4o-transcribe"
+    "openai/gpt-4o-mini-transcribe"
+    "openai/whisper-1"
+    "assembly/best"
+    "elevenlabs/scribe_v1"
+    "revai/machine" # please use --use_url=True
+    "revai/fusion" # please use --use_url=True
+    "speechmatics/enhanced"
+    "aquavoice/avalon-v1-en"
+)
+
+MAX_WORKERS=10
+
+num_models=${#MODEL_IDs[@]}
+
+for (( i=0; i<${num_models}; i++ ));
+do
+    MODEL_ID=${MODEL_IDs[$i]}
+    python run_eval.py \
+        --dataset_path="hf-audio/asr-leaderboard-longform" \
+        --dataset="earnings21" \
+        --split="test" \
+        --model_name ${MODEL_ID} \
+        --max_workers ${MAX_WORKERS}
+
+    python run_eval.py \
+        --dataset_path="hf-audio/asr-leaderboard-longform" \
+        --dataset="earnings22" \
+        --split="test" \
+        --model_name ${MODEL_ID} \
+        --max_workers ${MAX_WORKERS}
+
+    python run_eval.py \
+        --dataset_path="hf-audio/asr-leaderboard-longform" \
+        --dataset="tedlium" \
+        --split="test" \
+        --model_name ${MODEL_ID} \
+        --max_workers ${MAX_WORKERS}
+        
+    # Evaluate results
+    RUNDIR=`pwd` && \
+    cd ../normalizer && \
+    python -c "import eval_utils; eval_utils.score_results('${RUNDIR}/results', '${MODEL_ID}')" && \
+    cd $RUNDIR
+
+done
